414173da9896e8c6584bafd55da9e0112255efde
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-09-01 23:17:03 +0900

    aes

--- equally-spaced.el
+++ equally-spaced.el
@@ -1,28 +1,54 @@
-(defvar equally-spaced-width 4)
+;;; package -- Summary
+;;; Make empty lines equal height.
+
+
+;;; Commentary:
+
+
+;;; Code:
+
+
+(defcustom equally-spaced-width 2
+	"Buffer between lines."
+	:safe t)
+
+
 (defun equally-spaced-blank-line-p ()
   (string-match
    "^[ \t]*$" (buffer-substring
 	       (line-beginning-position)
 	       (line-end-position))))
+
+
 (defun equally-spaced-contents-line-p ()
   (not (equally-spaced-blank-line-p)))
+
+
 (defun equally-spaced-goto-next (predicate)
   (let ((buf 0))
     (while (and (= buf 0)
 		(not (funcall predicate)))
       (setq buf (forward-line)))))
+
+
 (defun equally-spaced-goto-top ()
   (goto-char (point-min))
   (equally-spaced-goto-next
    (function equally-spaced-contents-line-p)))
+
+
 (defun equally-spaced-goto-next-blank ()
   (beginning-of-line)
   (equally-spaced-goto-next
    (function equally-spaced-blank-line-p)))
+
+
 (defun equally-spaced-goto-next-contents ()
   (beginning-of-line)
   (equally-spaced-goto-next
    (function equally-spaced-contents-line-p)))
+
+
 (defun equally-spaced-make-gap-buffer ()
   (interactive)
   (save-excursion
@@ -46,6 +72,5 @@
 	  (setq buf (forward-line equally-spaced-width)))))))
 
 
-
-
 (provide 'equally-spaced)
+;;; equally-spaced.el ends here
